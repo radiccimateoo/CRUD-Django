ea35c982de185510c3c282af4ad9f4c62e59510b
Mateo <mateo@ejemplo.com>
2022-12-05 12:08:00 -0300

    funcionalidades modficar, insertar, borrar

--- peliculas/urls.py
+++ peliculas/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from .views import personaView
 
 urlpatterns = [
-    path('registrarPersona/', personaView.index),
+    path('registrarPersona/', personaView.registrar, name='registrar'),
     path('guardarPersona/', personaView.procesar),
+    path('', personaView.listar, name='listaPersonas'),
+    path('editarPersona/<int:id_persona>', personaView.editar, name='editar'),
+    path('actualizarPersona/<int:id_persona>', personaView.actulizar),
+    path('eliminarPersona/<int:id_persona>', personaView.eliminar, name='eliminar'),
 ]
--- peliculas/views.py
+++ peliculas/views.py
@@ -2,18 +2,50 @@ from django.shortcuts import render
 from django.http import HttpRequest
 
 from .forms import formularioPersona
+from .models import tablaPersona
 
 # Create your views here.
 class personaView(HttpRequest):
 
-    def index(request):
+    def registrar(request):
         persona = formularioPersona()
-        return render(request, 'documento.html', {'form': persona})
+        
+        return render(request, 'registrarPersona.html', {'form': persona})
 
     def procesar(request):
         persona = formularioPersona(request.POST)
+        
         if persona.is_valid():
             persona.save()
             persona = formularioPersona()
         
-        return render(request, 'documento.html', {'form':persona, 'mensaje':'ok'})
+        return render(request, 'registrarPersona.html', {'form':persona, 'mensaje':'ok'})
+
+    def editar(request, id_persona):
+        persona = tablaPersona.objects.filter(id= id_persona).first()
+        form = formularioPersona(instance= persona)
+        
+        return render(request, 'editarPersona.html', {'form':form, 'persona':persona})
+
+    def listar(request):
+        persona = tablaPersona.objects.all()
+
+        return render(request, 'listaPersonas.html', {'personas':persona})
+
+    def actulizar(request, id_persona):
+        persona =tablaPersona.objects.get(pk=id_persona)
+        form = formularioPersona(request.POST, instance= persona)
+        
+        if form.is_valid():
+            form.save()
+        
+        personas = tablaPersona.objects.all()
+
+        return render(request, 'listaPersonas.html', {'personas': personas})
+
+    def eliminar(request, id_persona):
+        persona = tablaPersona.objects.get(pk=id_persona)
+        persona.delete()
+        personas = tablaPersona.objects.all()
+
+        return render(request, 'listaPersonas.html', {'personas': personas, 'mensaje':'ok'})
